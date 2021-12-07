c54e9b8a61172d77171ab461fa644a28238454db
johan anzola <johan.anzola.hernandez@gmail.com>
2021-12-06 23:37:07 -0500

    Añadiendo Factura-ms

---
+++ facturacion_ms/src/main/java/com/misiontic/facturacion_ms/FacturacionMsApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.facturacion_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionMsApplication.class, args);
+	}
+
+}
---
+++ facturacion_ms/src/main/java/com/misiontic/facturacion_ms/models/Product.java
@@ -0,0 +1,48 @@
+package com.misiontic.facturacion_ms.models;
+import org.springframework.data.annotation.Id;
+public class Product {
+    @Id
+    private String idproduct;
+    private String name;
+    private Integer amount;
+    private Integer value;
+
+    public Product(String idproduct, String name, Integer amount, Integer value){
+        this.idproduct = idproduct;
+        this.name = name;
+        this.amount = amount;
+        this.value = value;
+    }
+
+    public String getIdproduct() {
+        return idproduct;
+    }
+
+    public void setIdproduct(String idproduct) {
+        this.idproduct = idproduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
