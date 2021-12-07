4b2fadab311961c77904e196b766ff58d801fcc9
David McEwen Arango <dmcewena@hotmail.com>
2021-12-07 14:18:12 -0500

    base para realizar api gate-way

--- api_gateway_nous/src/server.js
+++ api_gateway_nous/src/server.js
@@ -1,2 +1,5 @@
-module.export = {
-}
+module.exports = {
+    auth_api_url: 'https://nous-usuarios-ms.herokuapp.com/',
+    proveedores_api_url: 'https://proveedoresnous-ms.herokuapp.com/',
+    facturas_api_url: 'https://nous-factura-ms.herokuapp.com/'
+    };
